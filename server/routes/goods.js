f6bc7960dd954cbf5db542bc5b452c7af05ab786
SY930 <867598193@qq,com>
2018-07-23 10:13:31 +0800

    购物车(简单实现)/登陆、查询类表

--- server/routes/goods.js
+++ server/routes/goods.js
@@ -22,9 +22,39 @@ mongoose.connection.on('disconnected',function () {
   console.log("MongDB connected disconnected");
 });
 
-//查询
+//查询商品列表
 router.get('/',function (req,res,next) {
- Goods.find({},function (err,doc) {
+  let page = parseInt(req.param("page"));
+  let pageSize = parseInt(req.param("pageSize"));
+  let priceLevel = req.param('priceLevel');
+  let sort = req.param('sort');
+  let skip = (page-1)*pageSize;
+  var priceGt = '',priceLte='';
+  let params = {};
+  if(priceLevel!=='all'){
+    switch (priceLevel){
+      case "0":priceGt = 0;priceLte =500;break;
+      case "1":priceGt = 500;priceLte =1000;break;
+      case "2":priceGt = 1000;priceLte =5000;break;
+
+    }
+    params = {
+      salePrice:{
+        $gt:priceGt,
+        $lte:priceLte
+      }
+    }
+  }
+
+
+
+
+  let goodsModel = Goods.find(params).skip(skip).limit(pageSize);
+  goodsModel.sort({"salePrice":sort})
+
+
+
+  goodsModel.exec(function (err,doc) {
    if(err){
      res.json({
        status:'1',
@@ -43,4 +73,75 @@ router.get('/',function (req,res,next) {
  })
 });
 
+//加入到购物车
+router.post("/addCart",function (req,res,next) {
+  var userId = '100000077',productId = req.body.productId
+  var User = require('../models/user')
+
+  User.findOne({userId:userId},function (err,userDoc) {
+    if(err){
+      res.json({
+        status:'1',
+        msg:err.message
+      })
+    }else {
+      console.log("userDoc:" + userDoc);
+      if(userDoc){
+        let goodsItem  = '';
+        userDoc.cartList.forEach(function (item) {
+          if(item.productId==productId){
+            goodsItem = item;
+            item.productNum++;
+          }
+        });
+        if(goodsItem){
+          userDoc.save(function (err2,doc2) {
+            if(err2){
+              res.json({
+                status:'1',
+                msg:err2.message
+              })
+            }else {
+              res.json({
+                status:'0',
+                mag:'',
+                result:'success'
+              })
+            }
+          })
+        }else {
+          Goods.findOne({"productId":productId},function (err1,doc) {
+            if(err1){
+              res.json({
+                status:'1',
+                msg:err.message
+              })
+            }else {
+              if(doc){
+                doc.productNum = 1;
+                doc.checked = 1;
+                userDoc.cartList.push(doc);
+                userDoc.save(function (err2,doc2) {
+                  if(err2){
+                    res.json({
+                      status:'1',
+                      msg:err2.message
+                    })
+                  }else {
+                    res.json({
+                      status:'0',
+                      mag:'',
+                      result:'success'
+                    })
+                  }
+                })
+              }
+            }
+          })
+        }
+
+      }
+    }
+  })
+})
 module.exports = router;
--- src/views/GoodsList.vue
+++ src/views/GoodsList.vue
@@ -1,15 +1,19 @@
 <template>
-  <div >
-   <nav-header></nav-header>
+  <div>
+    <nav-header></nav-header>
     <nav-bread>
-      <span >Good</span>
+      <span>Good</span>
     </nav-bread>
     <div class="accessory-result-page accessory-page">
       <div class="container">
         <div class="filter-nav">
           <span class="sortby">Sort by:</span>
           <a href="javascript:void(0)" class="default cur">Default</a>
-          <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
+          <a href="javascript:void(0)" class="price" @click="sortGoods">Price
+            <svg class="icon icon-arrow-short">
+              <use xlink:href="#icon-arrow-short"></use>
+            </svg>
+          </a>
           <a href="javascript:void(0)" class="filterby stopPop" @click="showFilterPop">Filter by</a>
         </div>
         <div class="accessory-result">
@@ -17,9 +21,11 @@
           <div class="filter stopPop" id="filter" :class="{'filterby-show':filterBy}">
             <dl class="filter-price">
               <dt>Price:</dt>
-              <dd><a href="javascript:void(0)" :class="{'cur':priceChecked==='all'}" @click="priceChecked='all'">All</a></dd>
-              <dd v-for="(price,index) in priceFilter" >
-                <a href="javascript:void(0)" @click="setPriceFilter(index)" :class="{'cur':priceChecked==index}">{{price.startPrice}} - {{price.endPrice}}</a>
+              <dd><a href="javascript:void(0)" :class="{'cur':priceChecked==='all'}" @click="priceChecked='all'">All</a>
+              </dd>
+              <dd v-for="(price,index) in priceFilter">
+                <a href="javascript:void(0)" @click="setPriceFilter(index)"
+                   :class="{'cur':priceChecked==index}">{{price.startPrice}} - {{price.endPrice}}</a>
               </dd>
             </dl>
           </div>
@@ -30,86 +36,139 @@
               <ul>
                 <li v-for="(item,index) in goodsList">
                   <div class="pic">
-                    <a href="#"><img v-lazy="'/static/'+item.prodcutImg" alt=""></a>
+                    <a href="#"><img v-lazy="'/static/'+item.productImage" alt=""></a>
                   </div>
                   <div class="main">
                     <div class="name">{{item.productName}}</div>
-                    <div class="price">{{item.prodcutPrice}}</div>
+                    <div class="price">{{item.salePrice}}</div>
                     <div class="btn-area">
-                      <a href="javascript:;" class="btn btn--m">加入购物车</a>
+                      <a href="javascript:;" class="btn btn--m" @click="addCart(item.productId)">加入购物车</a>
                     </div>
                   </div>
                 </li>
               </ul>
+              <div class="view-more-normal" v-infinite-scroll="loadMore" infinite-scroll-disabled="busy"
+                   infinite-scroll-distance="10">
+                <img v-show="loading" src="./../assets/loading-spinning-bubbles.svg" alt="">
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
     <div class="md-overlay " v-show="overLayFlag" @click="closePop"></div>
-   <NavFooter></NavFooter>
+    <NavFooter></NavFooter>
   </div>
 </template>
 
 <script>
-import '../assets/css/base.css'
-import '../assets/css/product.css'
-import NavHeader from '../components/NavHeader.vue'
-import NavFooter from '../components/NavFooter.vue'
-import NavBread from '../components/NavBread.vue'
-import {getGoods} from '../api'
+  import '../assets/css/base.css'
+  import '../assets/css/product.css'
+  import NavHeader from '../components/NavHeader.vue'
+  import NavFooter from '../components/NavFooter.vue'
+  import NavBread from '../components/NavBread.vue'
+  import {getGoods, addCarts} from '../api'
   export default{
     data(){
-        return {
-            goodsList:[],
-           priceFilter:[
-               {
-                startPrice:'0.00',
-                endPrice:'500.00'
-           },
-             {
-               startPrice:'500.00',
-               endPrice:'1000.00'
-             },
-             {
-               startPrice:'1000.00',
-               endPrice:'2000.00'
-             },
-           ],
-          priceChecked:'all',
-          filterBy:false,
-          overLayFlag:false
-        }
+      return {
+        goodsList: [],
+        sortFlag: true,
+        page: 1,
+        pageSize: 8,
+        busy: true,
+        priceFilter: [
+          {
+            startPrice: '0.00',
+            endPrice: '500.00'
+          },
+          {
+            startPrice: '500.00',
+            endPrice: '1000.00'
+          },
+          {
+            startPrice: '1000.00',
+            endPrice: '2000.00'
+          },
+        ],
+        priceChecked: 'all',
+        filterBy: false,
+        overLayFlag: false,
+        loading: false
+      }
     },
-      components:{
-          NavHeader,
-        NavBread,
-        NavFooter
-      },
+    components: {
+      NavHeader,
+      NavBread,
+      NavFooter
+    },
     created(){
-        this.getGoodsList()
+      this.getGoodsList()
     },
-    methods:{
-        getGoodsList(){
-          getGoods().then((res)=>{
-            if(res.status==0){
-              console.log(res);
-              this.goodsList = res.result
+    methods: {
+      getGoodsList(flag){
+        var param = {
+          page: this.page,
+          pageSize: this.pageSize,
+          sort: this.sortFlag ? 1 : -1,
+          priceLevel: this.priceChecked
+        };
+        this.loading = true
+        getGoods(param).then((res) => {
+          this.loading = false;
+          if (res.status == 0) {
+            if (flag) {
+              this.goodsList = this.goodsList.concat(res.result.list);
+              if (res.result.count == 0) {
+                this.busy = true;//禁用加载
+              } else {
+                this.busy = false
+              }
+            } else {
+              this.goodsList = res.result.list
+              this.busy = false
             }
 
-          })
-        },
+          } else {
+            this.goodsList = [];
+          }
+
+        })
+      },
       showFilterPop(){
-        this.filterBy=true;
-        this.overLayFlag=true
+        this.filterBy = true;
+        this.overLayFlag = true
       },
       closePop(){
-        this.filterBy=false;
-        this.overLayFlag=false
+        this.filterBy = false;
+        this.overLayFlag = false
       },
       setPriceFilter(index){
-          this.priceChecked = index;
-          this.closePop()
+        this.priceChecked = index;
+        this.page = 1;
+        this.getGoodsList()
+        this.closePop()
+      },
+      sortGoods(){
+        this.sortFlag = !this.sortFlag;
+        this.page = 1;
+        this.getGoodsList()
+      },
+      loadMore(){
+
+        this.busy = true;
+        setTimeout(() => {
+          this.page++;
+          this.getGoodsList(true)
+        }, 500);
+      },
+      addCart(productId){
+        addCarts(productId).then((res) => {
+          if (res.status == 0) {
+            alert("加入成功")
+          } else {
+            alert('msg:' + res.msg)
+          }
+        })
       }
     },
 
